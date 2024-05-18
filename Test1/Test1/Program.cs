6f2bfbb3b6ea87de440f0b4117ab6b691dc34d8e
Do Huu Hau <dhhsvip@gmail.com>
2024-05-18 21:43:59 +0700

    advanced c#

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+namespace Test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable a = new Hashtable();
+            Hashtable b = new Hashtable(10);
+            b.Add(1, 2);
+            b.Add("t la sieu nhan","holishit");
+            Hashtable c = new Hashtable(b);
+
+        }
+    }
+}
