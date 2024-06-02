c689dd5ebb876a5edd2aab603dcf5b52a879ef88
Do Huu Hau <dhhsvip@gmail.com>
2024-06-03 06:26:39 +0700

    advanced c#

--- Test1/Test1/Program.cs
+++ Test1/Test1/Program.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 namespace Test1
 {
     internal class Program
     {
+        public static void swap <t>(ref t a , ref t b)
+        {
+            t temp = a;
+            a = b;
+            b = temp;
+        }
         static void Main(string[] args)
         {
             ArrayList arr = new ArrayList(100);
@@ -21,28 +28,38 @@ namespace Test1
             e.Push(1);
             e.Push("kukiu");
             e.Push('k');
-            Console.WriteLine("so pt "+e.Count);
-            Console.WriteLine("so dau tien "+e.Peek());
-            Console.WriteLine("da lay ra "+e.Pop());
+            Console.WriteLine("so pt " + e.Count);
+            Console.WriteLine("so dau tien " + e.Peek());
+            Console.WriteLine("da lay ra " + e.Pop());
             Console.WriteLine("so dau tien " + e.Peek());
             Stack f = new Stack(arr);
             Stack g = new Stack(a);
-           // //SortedList (giong voi thg hashtable nhung phai co cach sap xep)
-           // /*SortedList h = new SortedList(arr);*//*ko the chay dc (Error	CS1503	Argument 1: cannot convert from 'System.Collections.ArrayList' to 'System.Collections.IDictionary')*/
-           // SortedList h = new SortedList(g,arrangeParameter());(True)
-           // h.Add(1, 2);
-           ///* h.Add("hihihi", "kiki");*///loi ngay day neu ko co cach sap xep
-           // Console.Write(h);
-           //Queue (First in first out)
-           Queue i = new Queue(arr);
+            // //SortedList (giong voi thg hashtable nhung phai co cach sap xep)
+            // /*SortedList h = new SortedList(arr);*//*ko the chay dc (Error	CS1503	Argument 1: cannot convert from 'System.Collections.ArrayList' to 'System.Collections.IDictionary')*/
+            // SortedList h = new SortedList(g,arrangeParameter());(True)
+            // h.Add(1, 2);
+            ///* h.Add("hihihi", "kiki");*///loi ngay day neu ko co cach sap xep
+            // Console.Write(h);
+            //Queue (First in first out)
+            Queue i = new Queue(arr);
             Queue j = new Queue(e);
-            j.Enqueue(100);           
+            j.Enqueue(100);
             j.Dequeue();
             Console.WriteLine(j.Peek());
             //BitArray
             BitArray M = new BitArray(10);/*Mac dinh la false*/
-            BitArray N = new BitArray(10,true);/*Mac dinh la true*/
-            
+            Dictionary<string, int> m = new Dictionary<string, int>();
+            m.Add("ht", 1234);
+            m.Add("hihi", 2345);
+            m.Add("kiki", 54435);
+            m.Add("siuu", 7);
+            foreach ( KeyValuePair<string, int> k in m)
+            {
+                Console.WriteLine(k.Key+" "+k.Value);
+            }
+            int l = 0;
+            int r = 6;
+            swap<int >(ref l, ref r);
         }
     }
 }
